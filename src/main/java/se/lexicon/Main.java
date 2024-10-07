b184db891ed0b2dd820492a57a4f83324d40588f
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-10-07 09:40:00 +0200

    init commit

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class Main {
+
+    // single line comment
+    /*
+        multi-line comment
+     */
+
+    // psvm + tab
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+
+}
