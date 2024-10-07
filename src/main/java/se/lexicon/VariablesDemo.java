ca270a3fb714e60a97e9f0d744ffed31f5e28c54
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-10-07 10:06:52 +0200

    variables in java

---
+++ src/main/java/se/lexicon/VariablesDemo.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+        long balance = 500000;
+        balance = 600000;
+        double area = 25.22;
+        float radius = 5.5f;
+        char grade = 'A';
+        char gradeUn = '\u0041';
+        int accountNumber = 123456;
+
+        boolean isLoggedIn; // default value for boolean is false
+
+        System.out.println("Balance: " + balance); // 600000
+    }
+
+}
